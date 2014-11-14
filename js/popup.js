350d81cee28a0420ea2821bcc5bb9b34d6f3eab4
Konrad Dzwinel <kdzwinel@gmail.com>
2014-11-14 03:38:34 +0100

    Basic bootstrap styling.

--- js/popup.js
+++ js/popup.js
@@ -5,8 +5,15 @@ chrome.storage.local.get(['user', 'activities'], function(data) {
   userName.innerText = data.user.display_name;
 
   (data.activities).forEach(function(item) {
+    var span = document.createElement('span');
+    span.classList.add('badge');
+    span.innerText = item.resharers + item.plus_oners + item.comments;
+
     var li = document.createElement('li');
+    li.classList.add("list-group-item");
     li.innerText = item.activity_title;
+
+    li.appendChild(span);
 
     activityList.appendChild(li);
   });
