3d6970628ffd2e7329159215506cfc22e1474c0f
Konrad Dzwinel <kdzwinel@gmail.com>
2014-11-14 02:39:40 +0100

    Third step - background page, storage

--- popup.js
+++ popup.js
@@ -1,14 +1,10 @@
-var gPlusId = '111285183943460335169';
-
 var userName = document.getElementById('name');
 var activityList = document.getElementById('list');
 
-GDE_API.getUser(gPlusId).then(function (data) {
-  userName.innerText = data.display_name;
-});
+chrome.storage.local.get(['user', 'activities'], function(data) {
+  userName.innerText = data.user.display_name;
 
-GDE_API.getUserActivities(gPlusId).then(function (items) {
-  items.forEach(function(item) {
+  (data.activities).forEach(function(item) {
     var li = document.createElement('li');
     li.innerText = item.activity_title;
 
